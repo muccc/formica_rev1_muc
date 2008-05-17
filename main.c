75f747d6958ecbdf5f09ddcc05ec1aaf750b97d0
aj204 <aj204@360de629-c246-0410-a933-801fb8261919>
2008-05-17 12:59:49 +0000

    Modified clock so ACLK is using VLOCLK and this is divided by 8 before being used.
    git-svn-id: svn+ssh://forge.ecs.soton.ac.uk/projects/formica/firmware/trunk@276 360de629-c246-0410-a933-801fb8261919

--- main.c
+++ main.c
@@ -37,7 +37,11 @@ void init(void)
 	/* Use a 16 MHz clock (DCO) */
 	DCOCTL = CALDCO_16MHZ;
 	BCSCTL1 &= ~0x0f;
-	BCSCTL1 |= CALBC1_16MHZ;
+	BCSCTL1 |= 
+		/*XT2O=0: XT2 is on*/
+		/*XTS=0: LFXT1 mode select. 0 -Low frequency mode*/
+		DIVA_3 /* ACLK Divider 3: /8 */
+		|CALBC1_16MHZ; /* BCSCTL1 Calibration Data for 16MHz */
 
 	BCSCTL2 = SELM_DCOCLK	/* MCLK from DCO */
 		/* DIVMx = 0 : No MCLK divider */
@@ -45,6 +49,8 @@ void init(void)
 		/* DIVSx = 0 : No SMCLK divider */
 		/* DCOR = 0 : DCO internal resistor */;
 
+	BCSCTL3 = LFXT1S1; /*VLOCLK */
+	
 	opamp1_init();
 	bias_init();
 	adc10_init();
