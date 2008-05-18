c60280c873a210599ea534e0926d2b3056062e44
aj204 <aj204@360de629-c246-0410-a933-801fb8261919>
2008-05-18 01:16:40 +0000

    food detection works a bit better, maybe increase the "gap"
    git-svn-id: svn+ssh://forge.ecs.soton.ac.uk/projects/formica/firmware/trunk@301 360de629-c246-0410-a933-801fb8261919

--- food.c
+++ food.c
@@ -3,17 +3,33 @@ Food sensor Code, for now just contains a callback function
 */
 #include "food.h"
 
+#define AVERAGE_SIZE 3
 
 uint8_t gotfood;
-uint16_t nofoodref;
-uint16_t lastdata;
+uint16_t average[AVERAGE_SIZE];
+uint16_t mavg;
+uint8_t averageindex;
 
+/*Need to implement a moving  average based on the last 1sec? any major change from this average will trigger the food detection*/
 void foodCallback(uint16_t data, uint8_t channel){
-	lastdata = data;
-	if(data+50 < nofoodref){
+	uint8_t i;
+	if(data+50 < mavg){ //checking that the average buffer is full
 		gotfood = 1;
 	}else{
 		gotfood = 0;
+	
+		/*Calculating moving average*/
+		average[averageindex] = data;
+		mavg = 0;
+		for(i=0; i<AVERAGE_SIZE; i++){
+			mavg = mavg+ average[i];
+		}
+		mavg = mavg/AVERAGE_SIZE;
+		/*not using modulo*/
+		averageindex++;
+		if(averageindex ==AVERAGE_SIZE ){
+			averageindex = 0;
+		}
 	}
 }
 
@@ -24,6 +40,4 @@ void food_init(void){
 	gotfood = 0;
 	fled_init();
 	fled_on();
-	while(lastdata ==0){};
-	nofoodref = lastdata;
 }
