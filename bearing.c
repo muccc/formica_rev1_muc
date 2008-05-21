bb82cb616bf12f10261e701656cde227614fd942
rds204 <rds204@360de629-c246-0410-a933-801fb8261919>
2008-05-21 20:21:25 +0000

    Continues last commit: Change "curbearing" to "bearing"
    git-svn-id: svn+ssh://forge.ecs.soton.ac.uk/projects/formica/firmware/trunk@418 360de629-c246-0410-a933-801fb8261919

--- bearing.c
+++ bearing.c
@@ -1,6 +1,7 @@
 #include "device.h"
 #include "motor.h"
-uint16_t curbearing = 0;
+
+uint16_t bearing = 0;
 uint16_t bearing_strength = 0;
 
 static uint16_t l = 0, r = 0;
@@ -11,23 +12,23 @@ void bearing_set(uint16_t a, uint16_t b, uint16_t c)
 	if(a < b)
 		if(a < c)
 		{
-			curbearing = 0;
+			bearing = 0;
 			bearing_strength = a;
 		}
 		else
 		{
-			curbearing = 240;
+			bearing = 240;
 			bearing_strength = c;
 		}
 	else
 		if(b < c)
 		{
-			curbearing = 120;
+			bearing = 120;
 			bearing_strength = b;
 		}
 		else
 		{
-			curbearing = 240;
+			bearing = 240;
 			bearing_strength = c;
 		}
 }
