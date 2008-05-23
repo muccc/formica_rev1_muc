ea3f6a33bf8e1bb06a49390d348e0d3ca54a8d46
se204 <se204@360de629-c246-0410-a933-801fb8261919>
2008-05-23 03:32:21 +0000

    Arg, fixed typos
    git-svn-id: svn+ssh://forge.ecs.soton.ac.uk/projects/formica/firmware/trunk@438 360de629-c246-0410-a933-801fb8261919

--- bearing.c
+++ bearing.c
@@ -3,7 +3,7 @@
 
 uint16_t bearing = 0;
 uint16_t bearing_strength = 0;
-uint16_t light_intensity = 0
+uint16_t light_intensity = 0;
 
 void bearing_set( uint16_t *pdr )
 {
@@ -11,8 +11,8 @@ void bearing_set( uint16_t *pdr )
 		if(pdr[0] < pdr[2])
 		{
 			bearing = 0;
-			light_intensity = pdf[0];
-			if pdr[1] > pdr[2]
+			light_intensity = pdr[0];
+			if (pdr[1] > pdr[2])
 				bearing_strength = pdr[1] - pdr[0];
 			else
 				bearing_strength = pdr[2] - pdr[0];
@@ -20,8 +20,8 @@ void bearing_set( uint16_t *pdr )
 		else
 		{
 			bearing = 240;
-			light_intensity = pdf[2];
-			if pdr[1] > pdr[0]
+			light_intensity = pdr[2];
+			if (pdr[1] > pdr[0])
 				bearing_strength = pdr[1] - pdr[2];
 			else
 				bearing_strength = pdr[0] - pdr[2];
@@ -30,8 +30,8 @@ void bearing_set( uint16_t *pdr )
 		if(pdr[1] < pdr[2])
 		{
 			bearing = 120;
-			light_intensity = pdf[1];
-			if pdr[2] > pdr[0]
+			light_intensity = pdr[1];
+			if (pdr[2] > pdr[0])
 				bearing_strength = pdr[2] - pdr[1];
 			else
 				bearing_strength = pdr[0] - pdr[1];
@@ -39,8 +39,8 @@ void bearing_set( uint16_t *pdr )
 		else
 		{
 			bearing = 240;
-			light_intensity = pdf[2];
-			if pdr[1] > pdr[0]
+			light_intensity = pdr[2];
+			if (pdr[1] > pdr[0])
 				bearing_strength = pdr[1] - pdr[2];
 			else
 				bearing_strength = pdr[0] - pdr[2];
