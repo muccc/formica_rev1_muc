8da883ba33d016f4b06168d11254daf80a152304
se204 <se204@360de629-c246-0410-a933-801fb8261919>
2008-05-23 03:11:01 +0000

    Improved strength code
    git-svn-id: svn+ssh://forge.ecs.soton.ac.uk/projects/formica/firmware/trunk@435 360de629-c246-0410-a933-801fb8261919

--- bearing.c
+++ bearing.c
@@ -10,22 +10,34 @@ void bearing_set( uint16_t *pdr )
 		if(pdr[0] < pdr[2])
 		{
 			bearing = 0;
-			bearing_strength = pdr[0];
+			if pdr[1] > pdr[2]
+				bearing_strength = pdr[1] - pdr[0];
+			else
+				bearing_strength = pdr[2] - pdr[0];
 		}
 		else
 		{
 			bearing = 240;
-			bearing_strength = pdr[2];
+			if pdr[1] > pdr[0]
+				bearing_strength = pdr[1] - pdr[2];
+			else
+				bearing_strength = pdr[0] - pdr[2];
 		}
 	else
 		if(pdr[1] < pdr[2])
 		{
 			bearing = 120;
-			bearing_strength = pdr[1];
+			if pdr[2] > pdr[0]
+				bearing_strength = pdr[2] - pdr[1];
+			else
+				bearing_strength = pdr[0] - pdr[1];
 		}
 		else
 		{
 			bearing = 240;
-			bearing_strength = pdr[2];
+			if pdr[1] > pdr[0]
+				bearing_strength = pdr[1] - pdr[2];
+			else
+				bearing_strength = pdr[0] - pdr[2];
 		}
 }
