2f6f052c97b0edf4adc5dadb64019c3ae957a0d3
rds204 <rds204@360de629-c246-0410-a933-801fb8261919>
2008-09-12 12:13:18 +0000

    fw: License it under the GPL.
    git-svn-id: svn+ssh://forge.ecs.soton.ac.uk/projects/formica/firmware/trunk@468 360de629-c246-0410-a933-801fb8261919

--- behav/watchdog.h
+++ behav/watchdog.h
@@ -1,3 +1,21 @@
+/*  Copyright 2008 Stephen English, Jeffrey Gough, Alexis Johnson, 
+        Robert Spanton and Joanna A. Sun.
+
+    This file is part of the Formica robot firmware.
+
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the Formica robot firmware.  
+    If not, see <http://www.gnu.org/licenses/>.  */
 /* Makes the robot move if  */
 #ifndef __WATCHDOG_H
 #define __WATCHDOG_H
