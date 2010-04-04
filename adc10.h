1636bcf416e501784c7c7a0debf0e216ea224598
Robert Spanton <rspanton@zepler.net>
2010-04-04 21:24:14 +0100

    Remove definition of adc10_stream, which doesn't exist.

--- adc10.h
+++ adc10.h
@@ -26,9 +26,6 @@ void adc10_init( void );
 /* Read the temperature. Must be after _init whilst ADC is disabled*/
 uint16_t adc10_readtemp( void );
 
-/* Configure the ADC ready to start streaming out in TX downtime */
-void adc10_stream( void );
-
 /* Grab a single reading */
 void adc10_grab( void );
 
